afe038a85394c6cf21195f52d48edee845b00113
Felipe Plets <felipe.plets@menvia.com>
2019-04-01 01:02:02 -0300

    First version

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = (extension) => {
+  const controller = {};
+
+  const request = require('request-promise');
+  const baseURL = extension.settings.url;
+
+  controller.send = async (registration) =>
+    request({
+      url: baseURL,
+      method: 'PUT',
+      body: JSON.stringify({
+        customerName: `${registration.user.first_name} ${
+          registration.user.last_name
+        }`,
+        customerEmail: registration.user.email,
+        customerPhone: registration.user.telephone,
+        customerBirth: registration.user.details.birthday.split('T')[0],
+        companyName: registration.user.details.company,
+        companyJob: registration.user.details.role,
+        preRegistration: registration._id,
+      }),
+    });
+
+  controller.sendFile = async (customerId, file) =>
+    request({
+      method: 'POST',
+      url: `${baseURL}Photo?customerId=${customerId}`,
+      body: file,
+    });
+  return controller;
+};
