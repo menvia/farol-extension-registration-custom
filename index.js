30bc709265d88ae7b1600c148818c6e0cc41c2b9
Carlos Arthur Petry <carlos.petry@menvia.com>
2019-04-30 19:26:26 -0400

    fix exception due to split() of undefined variable

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-module.exports = (extension) => {
+module.exports = extension => {
   const controller = {};
 
-  const request = require('request-promise');
+  const request = require("request-promise");
   const baseURL = extension.settings.url;
 
-  controller.send = async (registration) =>
+  controller.send = async registration =>
     request({
       url: baseURL,
-      method: 'PUT',
+      method: "PUT",
       body: JSON.stringify({
         customerName: `${registration.user.first_name} ${
           registration.user.last_name
         }`,
         customerEmail: registration.user.email,
         customerPhone: registration.user.telephone,
-        customerBirth: registration.user.details.birthday.split('T')[0],
+        customerBirth:
+          typeof registration.user.details.birthday === "string"
+            ? registration.user.details.birthday.split("T")[0]
+            : undefined,
         companyName: registration.user.details.company,
         companyJob: registration.user.details.role,
-        preRegistration: registration._id,
-      }),
+        preRegistration: registration._id
+      })
     });
 
   controller.sendFile = async (customerId, file) =>
     request({
-      method: 'POST',
+      method: "POST",
       url: `${baseURL}Photo?customerId=${customerId}`,
-      body: file,
+      body: file
     });
   return controller;
 };
